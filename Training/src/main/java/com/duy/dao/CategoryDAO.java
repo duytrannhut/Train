91bbe90a0f4ed7f3751c9b2b6531709ccaee6d27
hoanghuy23 <87136225+hoanghuy23@users.noreply.github.com>
2021-07-09 16:27:53 +0700

    Delete Training directory

--- Training/src/main/java/com/duy/dao/CategoryDAO.java
+++
@@ -1,9 +0,0 @@
-package com.duy.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.duy.entity.Category;
-
-public interface CategoryDao extends JpaRepository<Category, String>{
-
-}
